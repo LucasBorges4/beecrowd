cbd9fcaddbb949229b119b0d717997b048ffca41
LucasBorges4 <Lucasborges-2013@hotmail.com>
2023-02-27 13:38:04 -0300

    27/02

---
+++ Python/ad-hoc/2374.py
@@ -0,0 +1,6 @@
+#Desired Pressure
+pressao_desejada = int(input())
+#Bomb Pressure
+pressao_bomba = int(input())
+#display the result
+print(f"{pressao_desejada - pressao_bomba}")
---
+++ Python/ad-hoc/2413.py
@@ -0,0 +1,6 @@
+# num of people what clicked in third link
+num = int(input())
+# num of people what clicked in first link
+num *= 4
+#display the result
+print(num)
